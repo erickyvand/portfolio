ff695870944e1dc3575350f03b415b2d27a497a0
erickyvand <erickyvand@gmail.com>
2020-06-04 12:07:27 +0200

    feature(page1): add page 1

--- src/components/Navigation.js
+++ src/components/Navigation.js
@@ -1,42 +1,62 @@
 import React from 'react';
-import { Link } from 'react-router-dom';
+import { NavLink } from 'react-router-dom';
+import { Navbar, Container, Nav } from 'react-bootstrap';
+import '../styles/styles.css';
 
 const Navigation = () => {
 	return (
 		<div>
-			<nav className='navbar navbar-expand-lg navbar-light bg-dark mb-4'>
-				<div className='container'>
-					<div className='collapse navbar-collapse' id='navbarSupportedContent'>
-						<ul className='navbar-nav mr-auto'>
-							<li className='nav-item'>
-								<Link className='nav-link text-white' to='/'>
+			<Navbar bg='dark' expand='lg' fixed='top'>
+				<Container>
+					<Navbar.Toggle
+						aria-controls='basic-navbar-nav'
+						className='text-white'
+					/>
+					<Navbar.Collapse id='basic-navbar-nav'>
+						<Nav>
+							<Navbar.Brand>
+								<NavLink
+									exact
+									to='/'
+									activeClassName='active'
+									className='text-white'
+								>
 									Home
-								</Link>
-							</li>
-							<li className='nav-item'>
-								<Link className='nav-link text-white' to='/about'>
+								</NavLink>
+							</Navbar.Brand>
+							<Navbar.Brand>
+								<NavLink
+									to='/about'
+									activeClassName='active'
+									className='text-white'
+								>
 									About
-								</Link>
-							</li>
-							<li className='nav-item'>
-								<Link className='nav-link text-white' to='/work'>
+								</NavLink>
+							</Navbar.Brand>
+							<Navbar.Brand>
+								<NavLink
+									to='/work'
+									activeClassName='active'
+									className='text-white'
+								>
 									Work
-								</Link>
-							</li>
-						</ul>
-            <ul className='navbar-nav ml-auto'>
-
-						<li className='nav-item'>
-							<Link className='nav-link text-white' to='/contact'>
+								</NavLink>
+							</Navbar.Brand>
+						</Nav>
+						<Navbar.Brand className='ml-auto'>
+							<NavLink
+								to='/contact'
+								activeClassName='active'
+								className='text-white'
+							>
 								Contact
-							</Link>
-						</li>
-            </ul>
-					</div>
-				</div>
-			</nav>
+							</NavLink>
+						</Navbar.Brand>
+					</Navbar.Collapse>
+				</Container>
+			</Navbar>
 		</div>
 	);
-}
+};
 
 export default Navigation;
