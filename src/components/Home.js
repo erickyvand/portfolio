a9b94eaf99723a7e62c0b59992d1908c8f46e5a9
erickyvand <erickyvand@gmail.com>
2020-06-05 11:53:50 +0200

    feature(page 2): add page 2

--- src/components/Home.js
+++ src/components/Home.js
@@ -1,23 +1,27 @@
-import React, { useState } from 'react';
-import { Row, Col, Container, Button } from 'react-bootstrap';
+import React, { useState, useEffect } from 'react';
+import { Row, Col, Container, Button, Image } from 'react-bootstrap';
+import FadeIn from 'react-fade-in';
 import '../styles/styles.css';
-import Image from '../../assets/vand.jpg';
-import Pic from '../../assets/ericky.jpeg'
+import { firstElement, secondeElement, thirdElement } from '../data';
+import Photo from '../../assets/vand.jpg';
+import Pic from '../../assets/ericky.jpeg';
 
 const Home = () => {
-	const [state, setState] = useState(Image);
-	const handleClick = () => {
-		setState(Pic);
-	};
+
 	return (
 		<div>
 			<Container>
 				<Row>
 					<Col md={7} xs={12} sm={12} lg={7} xl={7}>
-						<Button onClick={handleClick}>Click</Button>
+						<FadeIn delay={2000} transitionDuration={5000}>
+							<div>{firstElement}</div>
+							<div className='mt-4'>{secondeElement}</div>
+							<div className='mt-4'>{thirdElement}</div>
+						</FadeIn>
 					</Col>
 					<Col md={5} xs={12} sm={12} lg={5} xl={5}>
-						<img src={state} className='rounded-circle profile-image' alt='' />
+						<h2 className='text-center'>Software Developer</h2>
+						<Image src={Photo} className='profile-image' alt='' roundedCircle />
 					</Col>
 				</Row>
 			</Container>
